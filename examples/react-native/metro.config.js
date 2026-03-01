1f2d865e34532d9dc54078dd901a6afec79f4f7a
rtman <ryanjtrann@gmail.com>
2026-02-28 20:52:33 -0800

    module resolution and cleanup

--- examples/react-native/metro.config.js
+++ examples/react-native/metro.config.js
@@ -3,9 +3,12 @@ const path = require("node:path");
 
 const config = getDefaultConfig(__dirname);
 
-const packageRoot = path.resolve(__dirname, "../../packages/universal-video-uploader");
+const repoRoot = path.resolve(__dirname, "../..");
+const packageRoot = path.resolve(repoRoot, "packages/universal-video-uploader");
 
-config.watchFolders = [packageRoot];
+config.watchFolders = [packageRoot, path.resolve(repoRoot, "node_modules")];
+
+config.resolver.unstable_enableSymlinks = true;
 
 config.resolver.nodeModulesPaths = [
 	path.resolve(__dirname, "node_modules"),
